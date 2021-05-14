866e68f6c1ffe5fd42a867562f1fc59409830b75
Rahul <rk2670382@gmail.com>
2021-05-15 00:20:46 +0530

    spin wheel control
    event listner is added which rotate the wheel randomly

--- script.js
+++ script.js
@@ -23,7 +23,8 @@ let game=new Phaser.Game(config);
      this.load.image('stand',"icon/stand.png");
      this.load.image('pin',"icon/pin.png");
  }
-function create(){
+function create()
+{
     // decide height and width of game using game and config object which we are using for our images
     let w= game.config.width;
     let h= game.config.height;
@@ -46,13 +47,47 @@ function create(){
     this.stand=this.add.sprite(315,292,'stand');
     // scaling stand
     this.stand.setScale(1.3);
-    // for hidden the unwanted part just use depth property which is by default=0; change into 1;
+    // for hidden the unwanted part just use depth property which is by default=0; change into 1,-1,0;
     
+    // add event listener pointerDown and function spinwheel
+    this.input.on("pointerdown",spinwheel,this);
+    // here this is used for the remove undefine error of spinwheel function here "pointer" means touch or click on the screen  
     
 }
 function update(){
     // now spin the wheel thats means by changing its angles. this can be done in update part bcz this angle is continuously changing
-    this.wheel.angle +=1;// this function rotate one degree angle
+  //  this.wheel.angle +=1;// this function rotate one degree angle for anti-clock rotation use -=1 insted of +;
    
     
 }
+
+// event listner function
+
+function spinwheel()
+{
+    console.log("time to spin the wheel");
+    /*this.wheel.angle +=1300;
+     this would show an error ->angle property was not define bcz this spin function is not a part of scene .
+     -> also it rotate the image very fast for this purpose we use another function /property called tween
+    */
+    let round=Phaser.Math.Between(2,10);
+        console.log(round);
+    let ran1 = Phaser.Math.Between(0,8)*42;
+        console.log(ran1);
+    let ran2=round*360+ran1;
+    let tween =this.tweens.add({
+        targets: this.wheel,
+        angle : ran2,
+        ease:"cubic.easeOut",
+        delay:2000,
+        duration:6000
+    });
+   
+}
+ /*
+     alpha:0.1 this property change the transparency only value from 1 - 0;
+    *-> now there is a problem the wheel does not stop slowely for this purpose we use (ease->property )
+    --> another problem the pin stop on same block always->for this purpose we are using random number generation;
+    ->
+    there is 8 blocks in circle then we generate a randon number between 0-8 and the wheel one rotation =360->360/8=42 aprox this will be the rotation of one color block after->>randomnumber*42 asign to the angle property;
+    */
